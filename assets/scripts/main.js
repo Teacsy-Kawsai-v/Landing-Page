03ba9ff20e1ab841d03452c57a2f8496fa56dfd6
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2022-11-23 02:43:22 -0500

    feat(idiom): added scripts for change language button and spanish index

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -22,4 +22,20 @@ const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click',linkAction))    
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+// Change Language form English to Spanish
+
+const check = document.querySelector(".check");
+
+check.addEventListener("click", idiom);
+
+function idiom(){
+    let id = check.checked;
+    if(id == true){
+        location.href="index-es.html";
+    }else {
+        location.href="index.html";
+
+    }
+}
